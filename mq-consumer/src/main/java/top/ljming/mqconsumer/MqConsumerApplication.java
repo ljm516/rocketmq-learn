e04bd5ac0ad548834947b48fecd4cb9668719ac7
jiangming li <lijiangming>
2018-12-08 11:40:31 +0800

    init consumer

---
+++ mq-consumer/src/main/java/top/ljming/mqconsumer/MqConsumerApplication.java
@@ -0,0 +1,12 @@
+package top.ljming.mqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqConsumerApplication.class, args);
+    }
+}
